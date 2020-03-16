5163585956bcb013d855483f1fda8795404628bb
floxxui <60339661+floxxui@users.noreply.github.com>
2020-03-16 12:13:07 +0100

    Update Program.cs

--- Slutprojekt Labyrinten/Program.cs
+++ Slutprojekt Labyrinten/Program.cs
@@ -6,7 +6,9 @@ namespace Slutprojekt_Labyrinten
     {
         static void Main(string[] args)
         {
-            string currentRoom = ""; //Rummet som man befinner sig i
+            string currentRoom = "starting room"; //Rummet som man befinner sig i
+
+            /* Bild på house layout finns i ibispaint x*/
 
             string[] roomDesc = 
             { 
@@ -24,24 +26,23 @@ namespace Slutprojekt_Labyrinten
 
             string[] investigateDesc =
             {
-                "You take a closer look at the cracks in the walls and the floor. However, you dont seem to find anything.", //starting room
+                "You take a closer look at the cracks in the walls and the floor. However, you dont seem to find anything.", //starting room :3
                 "room2", //living room
-                "room3", //bedroom
-                "room4", //kitchen
-                "room5", //basement
-                "room6", //walking closet
-                "room7", //main hall 
-                "room8", //library
+                "room3", //bedroom :3
+                "room4", //kitchen :3
+                "room5", //basement :3
+                "room6", //walking closet :3
+                "room7", //main hall :3
+                "room8", //library 
                 "room9", //gaming room
-                "room10" //hallway
+                "room10" //hallway :3
             };
-
+            
             while (true)
             {
                 if (currentRoom == "starting room")
                 {
-                    Console.WriteLine(currentRoom[0]);
-                    
+                    BASE_ROOM(roomDesc, 2, investigateDesc);
                 }
 
                 else if (currentRoom == "living room")
@@ -66,9 +67,9 @@ namespace Slutprojekt_Labyrinten
             }
         }
 
-        static string BASE_ROOM()
+        static string BASE_ROOM(string[] roomDesc, int roomNumber, string [] investigateDesc)
         {
-            Console.WriteLine();
+            Console.WriteLine(roomDesc[roomNumber]);
 
             Console.WriteLine("Would you like to investigate? (y/n)");
 
@@ -78,7 +79,7 @@ namespace Slutprojekt_Labyrinten
             {
                 if (playerAnswer.ToLower() == "y")
                 {
-
+                    Console.WriteLine(investigateDesc[roomNumber]);
                     break;
                 }
                 else if (playerAnswer.ToLower() == "n")
